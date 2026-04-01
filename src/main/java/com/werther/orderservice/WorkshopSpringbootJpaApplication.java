ab0c47ae1578ea96060df2c99071230550e2c882
Francisco Wherther <wsiriano83@gmail.com>
2026-04-01 20:07:45 -0300

    feat: initial spring boot project setup

---
+++ src/main/java/com/werther/orderservice/WorkshopSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.werther.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootJpaApplication.class, args);
+	}
+
+}
