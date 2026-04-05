937ea65a676aeba1c8e8e809e82b7e9b6d81274d
Francisco Wherther <wsiriano83@gmail.com>
2026-04-04 23:17:55 -0300

    feat: implement service layer and findById resource

--- src/main/java/com/werther/orderservice/repositories/UserRepository.java
+++ src/main/java/com/werther/orderservice/repositories/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.werther.orderservice.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
 }
