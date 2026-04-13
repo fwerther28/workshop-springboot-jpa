e7362388bf850bd04f168211f67dec756f14876d
Francisco Wherther <wsiriano83@gmail.com>
2026-04-13 18:03:59 -0300

    feat: implement OrderItem and OrderItemPK using Lombok annotations

---
+++ src/main/java/com/werther/orderservice/entities/pk/OrderItemPK.java
@@ -0,0 +1,29 @@
+package com.werther.orderservice.entities.pk;
+
+import java.io.Serializable;
+
+import com.werther.orderservice.entities.Order;
+import com.werther.orderservice.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+}
